e61b6e416b3af6b78c9582c2e714d2e05ac127fa
Nagaveninayak <nagaveninayakn@gmail.com>
2023-03-21 10:50:43 +0530

    naming convention and it method

--- src/components/Greet.describe.test.tsx
+++ src/components/Greet.describe.test.tsx
@@ -1,10 +1,26 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./Greet"
 
-describe.only('Greet', () => {
-    test('Testing the Greet with describe', () => {
+describe('Greet', () => {
+    /**skip it */
+    xit('Testing the Greet with describe xit', () => {
         render(<Greet />);
         const textElement = screen.getByText('Hello');
         expect(textElement).toBeInTheDocument();
+    })
+
+    /**run that particular code */
+    fit('Testing the Greet with describe fit', () => {
+        render(<Greet />);
+        const textElement = screen.getByText('Hello');
+        expect(textElement).toBeInTheDocument();
+    })
+
+    describe('Nested', () => {
+        it('Nested greet', () => {
+            render(<Greet />);
+            const textElement = screen.getByText('Hello');
+            expect(textElement).toBeInTheDocument();
+        })
     })
 })
